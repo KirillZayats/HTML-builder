518244878501e6516b9241e87193203f794768a5
Kirill <kirill.zayats.99@mail.ru>
2024-01-18 20:13:58 +0300

    feat: add solution for 04-copy-directory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+async function addData(pathFolder, pathFolderCopy) {
+  fs.readdir(pathFolder, { withFileTypes: true }).then((files) => {
+    files.forEach((file) => {
+      const pathFile = `${pathFolder}\\${file.name}`;
+      const pathFileCopy = `${pathFolderCopy}\\${file.name}`;
+      if (file.isFile()) {
+        fs.copyFile(pathFile, pathFileCopy);
+      } else {
+        fs.mkdir(pathFileCopy, { recursive: true });
+        addData(pathFile, pathFileCopy);
+      }
+    });
+  });
+}
+
+async function copyFolder(nameFolder) {
+  const pathFolder = path.join(__dirname, nameFolder);
+  const pathFolderCopy = path.join(__dirname, `${nameFolder}-copy`);
+  try {
+    await fs.rm(pathFolderCopy, { recursive: true, force: true });
+    await fs.mkdir(pathFolderCopy, { recursive: true });
+    await addData(pathFolder, pathFolderCopy);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+copyFolder('files');

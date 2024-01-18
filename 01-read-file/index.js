740f85ab7f960f5a6743f098c577fd02da21207f
Kirill <kirill.zayats.99@mail.ru>
2024-01-18 16:00:21 +0300

    refactor: change link to 'stdout' for 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,6 @@
 const fileStream = require('fs');
 const path = require('path');
-const { stdout } = process;
+const { stdout } = require('process');
 const filePath = path.join(__dirname, 'text.txt');
 const readableStream = fileStream.createReadStream(filePath, 'utf8');
 readableStream.on('data', (chunk) => stdout.write(chunk));

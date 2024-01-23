f497ad53fb3014c0c7bc77fcc30b6f8de9c9f6f5
Kirill <kirill.zayats.99@mail.ru>
2024-01-23 17:53:42 +0300

    refactor: change method for create path in 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,17 +3,16 @@ const fs = require('fs');
 const path = require('path');
 
 const buildFile = (nameFile, extnameFile, fromFolder, toFolder) => {
-  const pathBuild = path.join(
-    __dirname,
-    `${toFolder}\\${nameFile}${extnameFile}`,
-  );
+  let pathBuild = path.join(__dirname, toFolder);
+  pathBuild = path.join(pathBuild, `${nameFile}${extnameFile}`);
   const writeStream = fs.createWriteStream(pathBuild);
   const pathFromFolder = path.join(__dirname, fromFolder);
+
   filePromises
     .readdir(pathFromFolder, { withFileTypes: true })
     .then((files) => {
       files.forEach((file) => {
-        const fullName = `${pathFromFolder}\\${file.name}`;
+        const fullName = path.join(pathFromFolder, file.name);
         if (file.isFile() && fullName.includes(extnameFile)) {
           const readableStream = fs.createReadStream(fullName, 'utf8');
           readableStream.on('data', (chunk) => {
